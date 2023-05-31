49ccb7fbf41b5842c95b3426a9327609f8dc4c82
kautsar <kautsarteguh2002@gmail.com>
2023-05-31 16:42:34 +0700

    setting vite, jquery & dashboard user

---
+++ resources/js/mobile.js
@@ -0,0 +1,18 @@
+import jquery from "jquery";
+window.$ = jquery;
+
+function openNav() {
+    $("#mySidenav").css("width", "100%");
+}
+
+function closeNav() {
+    $("#mySidenav").css("width", "0");
+}
+
+$("#openSideNav").on("click", function () {
+    openNav();
+});
+
+$(".closeSideNav").on("click", function () {
+    closeNav();
+});
