7db0ebb652dfeef34bdbdd58363305c29277b3fe
resakastepme <88456993+resakastepme@users.noreply.github.com>
2023-07-09 14:09:00 +0700

    FIX WEB

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Auth\LoginController;
-use App\Http\Controllers\Admin\ProfileController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Admin\RoomController;
 use App\Http\Controllers\User\ComplaintController;
 use App\Http\Controllers\Admin\ComplaintController as AdminComplaintController;
